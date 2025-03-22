7a0f4c1b01a34788c539ae5478d7b1bd072f85a0
Curran <curran.kelleher@gmail.com>
2025-03-22 12:07:39 -0400

    Add core types

--- src/index.ts
+++ src/index.ts
@@ -27,6 +27,52 @@ export type VizFile = {
   text: string;
 };
 
-// Generates a random 8-character file ID.
-export const generateVizFileId = (): VizFileId =>
-  Math.random().toString(16).slice(2, 10);
+// VizId
+//  * Unique identifier string for a Viz.
+//  * Common between Info and Content for a given Viz.
+//  * This is a UUID v4 string with dashes removed (for ease of URL copying).
+export type VizId = string;
+
+// VizLicense
+//  * The license associated with a viz.
+//  * This is an SPDX License Identifier.
+//  * See "Identifier" column in https://spdx.org/licenses/
+export type VizLicense = string;
+
+// VizContent
+//  * The content of a viz.
+export type VizContent = {
+  // id
+  // * The viz that this content is associated with
+  id: VizId;
+
+  // `files`
+  //   * The files in the viz.
+  files: VizFiles;
+
+  // title
+  //  * The title of the viz, same as Info.title
+  //  * Tracked here so that it can be versioned
+  //  * Restoring an old version should restore its old title
+  title?: string;
+
+  // height
+  // * The customized height of the viz in pixels
+  // * Not defined if the user has not customized it
+  // * If not defined, the default height is used,
+  //   which is specified by defaultVizHeight.
+  height?: number;
+
+  // license
+  // * The customized license associated with this viz
+  // * Not defined if the user has not customized it
+  license?: VizLicense;
+
+  // `isInteracting`
+  //   * `true` when the user is interacting
+  //     via interactive code widgets (e.g. Alt+drag)
+  //     * Hot reloading is throttled when this is `true`.
+  //   * `false` or `undefined` when they are not (e.g. normal typing)
+  //     * Hot reloading is debounced when this is `false`.
+  isInteracting?: boolean;
+};

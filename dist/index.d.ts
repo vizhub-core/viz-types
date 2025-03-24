ba7b549b4ff16664dc57e8d53c2187f15d4c1923
Curran <curran.kelleher@gmail.com>
2025-03-24 13:11:44 -0400

    Don't track dist in git

--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-export type VizFileId = string;
-export type VizFiles = {
-  [fileId: VizFileId]: VizFile;
-};
-export type VizFile = {
-  name: string;
-  text: string;
-};
-export declare const generateVizFileId: () => VizFileId;
--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.generateVizFileId = void 0;
-// Generates a random 8-character file ID.
-var generateVizFileId = function () {
-  return Math.random().toString(16).slice(2, 10);
-};
-exports.generateVizFileId = generateVizFileId;
